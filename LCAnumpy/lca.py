c1b47684d9fe017ee0adc1e2e483a6c69aebf101
asudeeaydin <asudeeaydin@gmail.com>
2021-07-26 09:34:34 +0200

    Improved comments for understandability and small notation adjustments

--- LCAnumpy/lca.py
+++ LCAnumpy/lca.py
@@ -14,7 +14,7 @@ def infer(basis, stimuli, eta, lamb, nIter, adapt, coeffs=None, softThresh=0):
             basis: Dictionary used to represent stimuli. Should be arranged along rows.
             coeffs: Values to start pre-threshold dictionary coefficients at for all stimuli.
             stimuli: Goals for dictionary representation. Should be arranged along rows.
-            eta: Controls rate of inference.
+            eta: Controls rate of inference. Equals to 1/tau in 2018 Olshausen paper.
             thresh: Threshold used in calculation of output variable of model neuron.
             lamb: Minimum value for thresh.
             nIter: Numer of times to run inference loop.
@@ -22,7 +22,7 @@ def infer(basis, stimuli, eta, lamb, nIter, adapt, coeffs=None, softThresh=0):
             adapt: Amount to change thresh by per run.
 
         Results:
-            s: Post-threshold dictionary coefficients.
+            a: Post-threshold dictionary coefficients.
             u: Pre-threshold internal *voltage.*
             thresh: Final value of thresh variable.
                                                                                                                             
@@ -32,31 +32,52 @@ def infer(basis, stimuli, eta, lamb, nIter, adapt, coeffs=None, softThresh=0):
     numStim = stimuli.shape[0] # number of stimuli
     dataSize = basis.shape[1] # size of a dictionary element
 
-    #Initialize u and s
+    #Initialize u and a
     u = np.zeros((numStim, numDict))
     # Don't understand what this does yet
     if coeffs is not None:
         u[:] = np.atleast_2d(coeffs)
-    s = np.zeros_like(u)
+    a = np.zeros_like(u)
     ci = np.zeros((numStim, numDict))
 
-    # Calculate c: overlap of basis functions with each other minus identity
+    # Calculate G: overlap of basis functions with each other minus identity
     # Row-wise correlation matrix - identity matrix to eliminate self correlation
-    c = basis.dot(basis.T) - np.eye(numDict)
+    G = basis.dot(basis.T) - np.eye(numDict)
 
-    #b[i,j] is the overlap fromstimuli:i and basis:j
+    #b[i,j] is the overlap from stimuli:i and basis:j
     b = stimuli.dot(basis.T)
-    thresh = np.absolute(b).mean(1)
 
-    #Update u[i] and s[i] for nIter time steps
+    # todo ?
+    thresh = np.absolute(b).mean(1)
+    print(thresh)
+
+    #Update u[i] and a[i] for nIter time steps
     for kk in range(nIter):
         #Calculate ci: amount other neurons are stimulated times overlap with rest of basis
-        ci[:] = s.dot(c)
+        ci[:] = a.dot(G)
+        # Update u using Rozell et al. (2008) eqn.
         u[:] = eta*(b-ci)+(1-eta)*u
+
         if softThresh == 1:
-            s[:] = np.sign(u)*np.maximum(0.,np.absolute(u)-thresh[:,np.newaxis]) 
-        else:
-            s[:] = u
-            s[np.absolute(s) < thresh[:,np.newaxis]] = 0.
+            # shrinkage with thresh
+            a[:] = np.sign(u)*np.maximum(0.,np.absolute(u)-thresh[:,np.newaxis])
+        else: # hard thresh
+            a[:] = u
+            # Converts 'thresh' from 1D vector to 2D where each element is a row value.
+            # Compares every element of a row of 'a' with the element of the same row in 'thresh'.
+            # Hard threshold
+            a[np.absolute(a) < thresh[:,np.newaxis]] = 0.
+        # Multiply threshold values bigger than 'lamb' with 'adapt'
         thresh[thresh>lamb] = adapt*thresh[thresh>lamb]
-    return (s,u,thresh)
+
+        # Sof thresholding - asude
+        # a[:] = np.sign(u) * np.maximum(0., np.absolute(u) - lamb)
+
+
+
+    print(thresh)
+    # print(np.shape(a))
+    # print(np.shape(thresh[:,np.newaxis]))
+    # print(a[np.absolute(a) < thresh[:,np.newaxis]])
+    # print(thresh[:,np.newaxis])
+    return (a,u,thresh)
